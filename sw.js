8d43919ffab475629c6073a986f4b74c896bd5a9
Heeyeol, Kim <devkimhy@gmail.com>
2018-04-22 14:34:54 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,46 +1,27 @@
-/*
- *
- *  Push Notifications codelab
- *  Copyright 2015 Google Inc. All rights reserved.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License
- *
- */
-
-/* eslint-env browser, serviceworker, es6 */
-
 'use strict';
 
 self.addEventListener('push', function (event) {
-    console.log('[Service Worker] Push Received.');
-    console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
-
-    const title = 'HYEOL Push';
+    console.log('[Service Worker] Push Received.', event);
+    var data = JSON.parse(event.data.text());
+    if(data['noti_noshow']) return;
+    const title = data.title;
     const options = {
-        body: event.data.text(),
-        icon: 'images/saramin_128_blue.png',
-        badge: 'images/badge.png'
+        body: data.desc,
+        icon: data.image,
+        data: data
     };
-
+    if (data.badge) {
+        options.badge = data.badge;
+    }
+    if (data.big_image) {
+        options.image = data.big_image;
+    }
     event.waitUntil(self.registration.showNotification(title, options));
 });
 
 self.addEventListener('notificationclick', function (event) {
-    console.log('[Service Worker] Notification click Received.');
-
+    console.log('[Service Worker] Notification click Received.', event);
+    var url = event.notification.data.url;
     event.notification.close();
-
-    event.waitUntil(
-        clients.openWindow('https://developers.google.com/web/')
-    );
+    event.waitUntil(clients.openWindow(url));
 });
